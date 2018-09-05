ff7dfa53e848592bacd04882e7cddffca8cf5c32
Ewan Mellor <commits@ewanmellor.org>
2018-09-05 16:29:02 -0700

    Added WorldToScreenPointReflected.

--- Assets/Scripts/UnityUtils/CameraExtension.cs
+++ Assets/Scripts/UnityUtils/CameraExtension.cs
@@ -32,5 +32,28 @@ namespace EMUtils.UnityUtils
                 return null;
             }
         }
+
+
+        /// <summary>
+        /// Same as Camera.WorldToScreenPoint, except if the point is
+        /// behind the camera then reflect it in the camera's forward
+        /// plane first (with an epsilon to guarantee that the point
+        /// is in front).
+        /// </summary>
+        public static Vector2 WorldToScreenPointReflected(this Camera camera, Vector3 pos)
+        {
+            var camTrans = camera.transform;
+            var camPos = camTrans.position;
+            var camForward = camTrans.forward;
+            var posFromCam = pos - camPos;
+            var camDot = Vector3.Dot(camForward, posFromCam);
+            if (camDot <= 0)
+            {
+                var proj = camForward * camDot * 1.01f;
+                pos = camPos + (posFromCam - proj);
+            }
+
+            return RectTransformUtility.WorldToScreenPoint(camera, pos);
+        }
     }
 }
