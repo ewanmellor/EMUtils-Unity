b6a1744b474cd9a38e472f4dadc45725ac4f23bd
Ewan Mellor <commits@ewanmellor.org>
2018-09-06 00:21:45 -0700

    Added Vector2Extension.ProjectToRectEdge, MathfExtension.PI2.

--- Assets/Scripts/UnityUtils/MathfExtension.cs
+++ Assets/Scripts/UnityUtils/MathfExtension.cs
@@ -6,6 +6,7 @@ namespace EMUtils.UnityUtils
     public static class MathfExtension
     {
         public const float OneOverSqrt2 = 0.70710676908493f;
+        public const float PI2 = Mathf.PI / 2;
 
         public static float Quantized(this float i, float q = 5.0f)
         {
---
+++ Assets/Scripts/UnityUtils/Vector2Extension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+namespace EMUtils.UnityUtils
+{
+    public static class Vector2Extension
+    {
+        /// <summary>
+        /// Return a new point, derived from this point and the given rectangle,
+        /// where the given point is projected out from the center of the rectangle
+        /// to the nearest edge.
+        /// </summary>
+        public static Vector2 ProjectToRectEdge(this Vector2 pos,
+                                                float rectWidth, float rectHeight,
+                                                out float outAngleRad)
+        {
+            var center = new Vector2(rectWidth / 2f, rectHeight / 2f);
+            var normalizedDir = (pos - center).normalized;
+            var angleRad = Mathf.Atan2(normalizedDir.x, normalizedDir.y);
+            var distHeight = Mathf.Abs(center.y / Mathf.Cos(angleRad));
+            var distWidth = Mathf.Abs(center.x / Mathf.Cos(angleRad + MathfExtension.PI2));
+            var shortestDist = Mathf.Min(distHeight, distWidth);
+            outAngleRad = angleRad;
+            return center + (normalizedDir * shortestDist);
+        }
+    }
+}
