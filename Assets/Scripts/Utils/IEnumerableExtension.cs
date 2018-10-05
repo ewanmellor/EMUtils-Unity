8a38be063174e54a9a80d1f4bccadc5daf405d19
Ewan Mellor <commits@ewanmellor.org>
2018-10-05 13:46:29 -0700

    Added WhereNotNull, SelectNotNull.

--- Assets/Scripts/Utils/IEnumerableExtension.cs
+++ Assets/Scripts/Utils/IEnumerableExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -9,6 +10,25 @@ namespace EMUtils.Utils
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
         {
             return enumerable == null || !enumerable.Any();
+        }
+
+        public static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
+            this IEnumerable<TSource> source, Func<TSource, TResult> selector
+            ) where TResult : class
+        {
+            return source.Select(selector).WhereNotNull();
+        }
+
+        public static IEnumerable<TResult> SelectNotNull<TSource, TResult>(
+            this IEnumerable<TSource> source, Func<TSource, int, TResult> selector
+            ) where TResult : class
+        {
+            return source.Select(selector).WhereNotNull();
+        }
+
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> source) where T : class
+        {
+            return source.Where(x => x != null);
         }
     }
 }
