c0ce2f75124acdf19dfd9a1f22f6996a02926d3c
Ewan Mellor <commits@ewanmellor.org>
2018-10-07 13:19:48 -0700

    Added Color32Extension, with an AlphaOverlay method.
    Add a test framework, for unit testing AlphaOverlay.

---
+++ Assets/Scripts/UnityUtils/Color32Extension.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+namespace EMUtils.UnityUtils
+{
+    public static class Color32Extension
+    {
+        public static readonly Color32 Transparent = new Color32(0, 0, 0, 0);
+
+
+        public static Color32 AlphaOverlay(this Color32 back, Color32 front)
+        {
+            if (front.a == 0)
+            {
+                return back;
+            }
+            if (front.a == 255 || back.a == 0)
+            {
+                return front;
+            }
+            if (back.a == 255)
+            {
+                var frontA = front.a + 1;
+                var invFrontA = 256 - front.a;
+                return new Color32(
+                    (byte)((back.r * invFrontA + front.r * frontA) >> 8),
+                    (byte)((back.g * invFrontA + front.g * frontA) >> 8),
+                    (byte)((back.b * invFrontA + front.b * frontA) >> 8),
+                    (byte)255);
+            }
+            else
+            {
+                var frontA = front.a / 255f;
+                var invFrontA = 1f - frontA;
+                var backA = back.a / 255f;
+                var resultA = frontA + backA * invFrontA;
+
+                var resultR = (front.r / 255f) * frontA + (back.r / 255f * backA) * invFrontA;
+                var resultG = (front.g / 255f) * frontA + (back.g / 255f * backA) * invFrontA;
+                var resultB = (front.b / 255f) * frontA + (back.b / 255f * backA) * invFrontA;
+
+                return new Color32(
+                    (byte)Mathf.Round(255f * resultR / resultA),
+                    (byte)Mathf.Round(255f * resultG / resultA),
+                    (byte)Mathf.Round(255f * resultB / resultA),
+                    (byte)Mathf.Round(255f * resultA));
+            }
+        }
+    }
+}
---
+++ Assets/Tests/UnityUtils/Color32ExtensionTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+
+namespace EMUtils.UnityUtils
+{
+    public class Color32ExtensionTest
+    {
+        [Test]
+        public void TestAlphaOverlayOpaqueBack()
+        {
+            var back = new Color32(128, 64, 192, 255);
+            var front = new Color32(0, 255, 0, 64);
+            var expected = new Color32(96, 112, 144, 255);
+            Assert.AreEqual(expected, back.AlphaOverlay(front));
+        }
+
+        [Test]
+        public void TestAlphaOverlayOpaqueFront()
+        {
+            var back = new Color32(128, 64, 192, 28);
+            var front = new Color32(0, 255, 0, 255);
+            var expected = front;
+            Assert.AreEqual(expected, back.AlphaOverlay(front));
+        }
+
+        [Test]
+        public void TestAlphaOverlayTransparentBack()
+        {
+            var back = new Color32(128, 64, 192, 0);
+            var front = new Color32(0, 255, 0, 64);
+            var expected = front;
+            Assert.AreEqual(expected, back.AlphaOverlay(front));
+        }
+
+        [Test]
+        public void TestAlphaOverlayTransparentFront()
+        {
+            var back = new Color32(128, 64, 192, 28);
+            var front = new Color32(0, 255, 0, 0);
+            var expected = back;
+            Assert.AreEqual(expected, back.AlphaOverlay(front));
+        }
+
+        [Test]
+        public void TestAlphaOverlayTranslucentBack()
+        {
+            var back = new Color32(128, 64, 192, 128);
+            var front = new Color32(0, 255, 0, 64);
+            var expected = new Color32(77, 140, 115, 160);
+            Assert.AreEqual(expected, back.AlphaOverlay(front));
+        }
+    }
+}
