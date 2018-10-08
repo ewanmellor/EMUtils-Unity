e7bbb92508cfae66be1b652cc6bdc0d243dc8e1c
Ewan Mellor <commits@ewanmellor.org>
2018-10-07 18:54:24 -0700

    Added NotificationCenter.

---
+++ Assets/Scripts/NotificationCenter/NotificationCenter.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using EMUtils.Utils;
+
+
+namespace EMUtils.NotificationCenter
+{
+    public class NotificationCenter
+    {
+        public delegate void NotificationDelegate(IDictionary senderInfo);
+
+
+        static NotificationCenter _Instance;
+
+        public static NotificationCenter Instance
+        {
+            get
+            {
+                EMAssert.OnMainThread();
+
+                if (_Instance == null)
+                {
+                    _Instance = new NotificationCenter();
+                }
+                return _Instance;
+            }
+        }
+
+
+        readonly Dictionary<string, List<NotificationDelegate>> observers = new Dictionary<string, List<NotificationDelegate>>();
+
+
+        public void AddObserver(string name, NotificationDelegate del)
+        {
+            observers.AddToList(name, del);
+        }
+
+
+        public void RemoveObserver(string name, NotificationDelegate del)
+        {
+            observers.RemoveFromList(name, del);
+        }
+
+
+        public void Notify(string name, IDictionary senderInfo = null)
+        {
+            EMAssert.OnMainThread();
+
+            var obs = observers[name];
+            foreach (var ob in obs)
+            {
+                try
+                {
+                    ob(senderInfo);
+                }
+                catch (Exception err)
+                {
+                    Debug.LogErrorFormat("Squelching error in NotificationCenter.Notify: {0}", err);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Utils/DictionaryExtension.cs
+++ Assets/Scripts/Utils/DictionaryExtension.cs
@@ -1,14 +1,93 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace EMUtils.Utils
 {
     public static class DictionaryExtension
     {
-        public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue def = default(TValue))
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue def = default(TValue))
         {
             TValue result;
             return dict.TryGetValue(key, out result) ? result : def;
+        }
+
+
+        public static TValue GetValueOrPut<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, Func<TValue> generator)
+        {
+            TValue result;
+            if (dict.TryGetValue(key, out result))
+            {
+                return result;
+            }
+            result = generator();
+            dict[key] = result;
+            return result;
+        }
+
+
+        public static void AddToList<TKey, TValue>(this IDictionary<TKey, List<TValue>> dict, TKey key, TValue val)
+        {
+            var l = dict.GetValueOrPut(key, () => new List<TValue>());
+            l.Add(val);
+        }
+
+
+        /// <returns><c>true</c> if the given val was found and removed.</returns>
+        public static bool RemoveFromList<TKey, TValue>(this IDictionary<TKey, List<TValue>> dict, TKey key, TValue val)
+        {
+            var l = dict.GetValueOrDefault(key);
+            if (l == null)
+            {
+                return false;
+            }
+            var result = l.Remove(val);
+            if (l.Count == 0)
+            {
+                dict.Remove(key);
+            }
+            return result;
+        }
+
+
+        public static bool ContentEquals(this IDictionary dict, IDictionary other)
+        {
+            if (dict == null || other == null || dict.Count != other.Count)
+            {
+                return false;
+            }
+            foreach (DictionaryEntry kv in dict)
+            {
+                object otherV = other[kv.Key];
+                if (otherV == null || !kv.Value.Equals(otherV))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static bool ContentEquals<TKey, TValue>(this IDictionary<TKey, TValue> dict, IDictionary<TKey, TValue> other)
+        {
+            if (dict == null || other == null || dict.Count != other.Count)
+            {
+                return false;
+            }
+            foreach (var kv in dict)
+            {
+                TValue otherV;
+                if (!other.TryGetValue(kv.Key, out otherV))
+                {
+                    return false;
+                }
+                if (!kv.Value.Equals(otherV))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
