6791134a8c4792afe0c619b69db894babb91a35e
Ewan Mellor <commits@ewanmellor.org>
2019-06-26 00:28:14 -0700

    Added RectTransform.ContainsScreenPoint.

---
+++ Assets/Scripts/UnityUtils/RectTransformExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+namespace EMUtils.UnityUtils
+{
+    public static class RectTransformExtension
+    {
+        public static bool ContainsScreenPoint(this RectTransform rectTransform, Vector3 point)
+        {
+            var localPosition = rectTransform.InverseTransformPoint(point);
+            return rectTransform.rect.Contains(localPosition);
+        }
+    }
+}
