faa87436f4cf97d4abc0f293576b3cf675a26d12
Ewan Mellor <commits@ewanmellor.org>
2019-06-27 13:52:17 -0700

    Added.

---
+++ Assets/Scripts/UnityUtils/RectExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace EMUtils.UnityUtils
+{
+	public static class RectExtension
+	{
+		public static bool ContainsXZ(this Rect rect, Vector3 point)
+		{
+			return rect.Contains(new Vector2(point.x, point.z));
+        }
+	}
+}
