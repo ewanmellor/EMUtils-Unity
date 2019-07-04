4efeb40fd986c81cbd9a2579dea15238bd86a0f6
Ewan Mellor <commits@ewanmellor.org>
2019-07-03 17:15:28 -0700

    Added Vector3Extension.CrossNormalized.

--- Assets/Scripts/UnityUtils/Vector3Extension.cs
+++ Assets/Scripts/UnityUtils/Vector3Extension.cs
@@ -5,6 +5,9 @@ namespace EMUtils.UnityUtils
 {
     public static class Vector3Extension
     {
+        const float kEpsilonSquared = Vector3.kEpsilon * Vector3.kEpsilon;
+
+
         public static Vector2 WithoutZ(this Vector3 v)
         {
             return new Vector2(v.x, v.y);
@@ -13,6 +16,28 @@ namespace EMUtils.UnityUtils
         public static Vector3 Quantized(this Vector3 v, float q = 5.0f)
         {
             return new Vector3(v.x.Quantized(q), v.y.Quantized(q), v.z.Quantized(q));
+        }
+
+
+        /// <returns>Vector3.Cross(a, b).normalized, except without allocating the temporary.</returns>
+        public static Vector3 CrossNormalized(Vector3 a, Vector3 b)
+        {
+            float cx = a.y * b.z - a.z * b.y;
+            float cy = a.z * b.x - a.x * b.z;
+            float cz = a.x * b.y - a.y * b.x;
+
+            float sqrMag = cx * cx + cy * cy + cz * cz;
+            if (sqrMag <= kEpsilonSquared)
+            {
+                return Vector3.zero;
+            }
+
+            float mag = Mathf.Sqrt(sqrMag);
+            return new Vector3(
+                cx / mag,
+                cy / mag,
+                cz / mag
+                );
         }
 
 
