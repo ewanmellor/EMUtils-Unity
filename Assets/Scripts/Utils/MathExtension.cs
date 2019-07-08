a2af0c338ec9e1459f21ced58bf0b4fdb0eba83d
Ewan Mellor <commits@ewanmellor.org>
2019-07-08 12:43:21 -0700

    Added MathExtension.Clamp.

---
+++ Assets/Scripts/Utils/MathExtension.cs
@@ -0,0 +1,14 @@
+﻿public static class MathExtension
+{
+#if NETCOREAPP2_0 || NETSTANDARD2_1
+#else
+    public static int Clamp(int value, int min, int max)
+    {
+        return (
+            value <= min ? min :
+            value >= max ? max :
+            value
+            );
+    }
+#endif
+}
